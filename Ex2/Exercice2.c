5d678e841a6f11099185946cb479ab69189f8996
Student <issam.melloukifilali@student.hepl.be>
2026-02-12 14:58:28 +0100

    Etape 3, 4 fini

--- Ex2/Exercice2.c
+++ Ex2/Exercice2.c
@@ -9,19 +9,22 @@
 #include <stdio.h>
 #include <signal.h>
 #define trace(message) printf("THREAD[%d, %p]: %s\n", getpid(), pthread_self(), message)
-#define resultat(res) printf("THREAD[%d, %p]: Nombre d'occurence: %d\n", getpid(), pthread_self(), res)
+#define resultat(message,res) printf("THREAD[%d, %p]: %s %d\n", getpid(), pthread_self(),message, res)
 void* Fct1(void* arg);
 void* Fct2(void* arg);
+void Fct2Fin(void* arg);
 void* Fct3(void* arg);
 void* Fct4(void* arg);
 void HandlerSIGINT(int sig);
+void HandlerSIGUSR1(int sig);
 typedef struct
 {
 	char fich[20];
 	char lire[20];
 	int tab;
 }FICHIER;
-pthread_t th1, th2, th3, th4;
+pthread_t th1, th2, th3, th4, master;
+int loop = 1;
 int main()
 {
 	trace("Thread principal commence");
@@ -30,7 +33,11 @@ int main()
 	A.sa_flags = 0;
 	sigemptyset(&A.sa_mask);
 	sigaction(SIGINT, &A, NULL);
+	A.sa_handler = HandlerSIGUSR1;
+	sigaction(SIGUSR1, &A, NULL);
 	trace("Armement signal SIGINT");
+	trace("Creation du thread master");
+	pthread_create(&master, NULL, Fct2, NULL);
 	trace("Creation Thread secondaire 1");
 	pthread_create(&th1, NULL, Fct1, NULL);
 	trace("Creation Thread secondaire 2");
@@ -40,23 +47,74 @@ int main()
 	trace("Creation Thread secondaire 4");
 	pthread_create(&th4, NULL, Fct1, NULL);
 
-	trace("Attente");
-	pause();
+	trace("Attente des threads slaves et master");
+	sigset_t mask;
+	sigfillset(&mask);
+	pthread_sigmask(SIG_SETMASK, &mask, NULL);
+	pthread_join(th1, NULL);
+	pthread_join(th2, NULL);
+	pthread_join(th3, NULL);
+	pthread_join(th4, NULL);
+	trace("Demande d'annulation au master");
+	pthread_cancel(master);
 	trace("Je me termine (thread principal)");
 	pthread_exit(NULL);
 }
 void* Fct1(void* arg)
 {
-	trace("Thread secondaire commence");
-	// sigset_t mask;
-	// sigfillset(&mask);
-	// pthread_sigmask(SIG_SETMASK, &mask, NULL);
+	int i = 1000;
+	if (pthread_self() == th1)
+		i = 1;
+	else if (pthread_self() == th2)
+		i = 2;
+	else if (pthread_self() == th3)
+		i = 3;
+	else 
+		i = 4;
+	resultat("Thread secondaire", i);
+	sigset_t mask;
+	sigfillset(&mask);
+	sigdelset(&mask, SIGUSR1);
+	pthread_sigmask(SIG_SETMASK, &mask, NULL);
+	trace("Je masque tout sauf SIGUSR1"); // etape 3
 	trace("Attente d'un signal..");
 	pause();
-	trace("Je me termine");
+	resultat("Je me termine (Thread secondaire):", i);
 	pthread_exit(NULL);
 }
-void HandlerSIGINT(int s)
+void* Fct2(void* s) // etape 2
 {
+	trace("Thread master commence");
+	trace("Attente d'un signal..");
+	pthread_cleanup_push(Fct2Fin, (void*) "Thread Master");
+	sigset_t mask;
+	sigfillset(&mask);
+	sigdelset(&mask, SIGINT);
+	pthread_sigmask(SIG_SETMASK, &mask, NULL);
+	trace("Je masque tout sauf SIGINT"); // etape 3
+	while(1)
+	{
+		pause();
+		trace("Thread Master recoit le signal SIGINT");
+		pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL); // etape 4
+		pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS, NULL);
+	}
+	pthread_cleanup_pop(1);
+}
+void HandlerSIGINT(int s) // etape 1
+{
+	loop = 0;
 	trace("J'ai recu le signal SIGINT");
+	kill(getpid(), SIGUSR1);
+}
+void HandlerSIGUSR1(int s) // etape 3
+{
+	trace("J'ai recu le signal SIGUSR1");
+}
+void Fct2Fin(void* s) // etape 4
+{
+	const char* p = (const char*) s;
+	char txt[80];
+	sprintf(txt, "%s 	: Je passe par ma fonction de terminaison\n", p);
+	trace(txt);
 }
