9915f10dd50d9a383894d201038b9454a97be426
Student <issam.melloukifilali@student.hepl.be>
2026-02-20 18:45:47 +0100

    Bonus réussi

--- Ex3/Exercice3.c
+++ Ex3/Exercice3.c
@@ -8,14 +8,21 @@
 #include <string.h>
 #include <stdio.h>
 #include <signal.h>
+
 #define trace(message) printf("Thread %d.%lu: %s\n", getpid(),(unsigned long) pthread_self(), message)
 #define resultat(message,res) printf("Thread %d.%lu: %s %d\n", getpid(),(unsigned long) pthread_self(),message, res)
+
 void* Fct1(void* arg);
 void Attente(int milli);
+void LiberationCompteur(void *p);
+void HandlerSIGINT(int sig);
+
 pthread_mutex_t mutexParam;
-int compteur = 0;
 pthread_mutex_t mutexCompteur;
 pthread_cond_t condCompteur;
+pthread_key_t cleCompteur;
+
+int compteur = 0;
 typedef struct
 {
 	char nom[20];
@@ -29,11 +36,19 @@ DONNEE data[] = {
 	"", 0 
 };
 pthread_t th[100];
-int loop = 1;
 int main()
 {
 	int i;
 	trace("Thread principal commence");
+	struct sigaction A;
+	A.sa_handler = HandlerSIGINT; // etape 4
+	A.sa_flags = 0;
+	sigemptyset(&A.sa_mask);
+	sigaction(SIGINT, &A, NULL);
+	sigset_t mask;
+	sigfillset(&mask);
+	pthread_sigmask(SIG_SETMASK, &mask, NULL);
+	pthread_key_create(&cleCompteur, LiberationCompteur);// etape 4
 	pthread_mutex_init(&mutexCompteur, NULL);
 	pthread_cond_init(&condCompteur, NULL);
 	for (i = 0; data[i].nbSecondes != 0; i++)
@@ -69,8 +84,15 @@ int main()
 void* Fct1(void* arg)
 {
 	pthread_mutex_lock(&mutexParam);
-	trace("se lance.");
+	sigset_t mask;
+	sigfillset(&mask);
+	sigdelset(&mask, SIGINT);
+	pthread_sigmask(SIG_SETMASK, &mask, NULL);// etape 4
 	DONNEE *d = (DONNEE *) arg;
+	char* nom = (char*) malloc(sizeof(d->nom));
+	strcpy(nom, d->nom);
+	pthread_setspecific(cleCompteur, (void *) nom);// etape 4
+	trace("se lance.");
 	trace(d->nom);
 	Attente(d->nbSecondes);
 	pthread_mutex_unlock(&mutexParam);
@@ -86,5 +108,23 @@ void Attente(int milli)
 	struct timespec ts;
 	ts.tv_sec = milli;
 	ts.tv_nsec = 0;
-	nanosleep(&ts, NULL);
+	struct timespec rem;// etape 4 (pour les autres étapes c'est: nanosleep(&ts, NULL) tout seul sans while et errno)
+	while(nanosleep(&ts, &rem) == -1 && errno == EINTR)
+	{
+		char txt[100];
+        sprintf(txt, "Interrompu, Temps restant: %ld secondes",rem.tv_sec);
+        trace(txt);
+		ts = rem;
+	}
+}
+void LiberationCompteur(void *p)
+{
+	free(p);
+}
+void HandlerSIGINT(int sig)
+{
+	char* pNom = (char *)pthread_getspecific(cleCompteur);
+	char txt[80];
+	sprintf(txt, "s'occupe de << %s >>", pNom);
+	trace(txt);
 }
